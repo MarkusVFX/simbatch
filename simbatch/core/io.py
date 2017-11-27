e6bd50f4efe6b438777d5a6016d35ee38edca10b
MarkusVFX <vfx@mqs.pl>
2017-11-27 20:34:35 +0000

    io PEP8

--- simbatch/core/io.py
+++ simbatch/core/io.py
@@ -12,7 +12,8 @@ class InOutStorage:
         self.p = batch.p
         self.s = batch.s
 
-    def is_absolute(self,path):
+    @staticmethod
+    def is_absolute(path):
         if len(path) > 2:
             if path[1] == ":" and path[2] == "\\":
                 return 1
@@ -20,37 +21,41 @@ class InOutStorage:
                 return 1
         return 0
 
-    def get_flat_name(self, name):
+    @staticmethod
+    def get_flat_name(name):
         return re.sub('\s', '_', name)
 
-    def get_path_from_full(self, full):
+    @staticmethod
+    def get_path_from_full(full):
         return os.path.dirname(full)
 
-    def create_directory(self, dir):
-        if len(dir) > 0:
-            if not os.path.exists(dir):
-                if dir[1] == ":":
-                    check_drive = dir[0] + ":\\"
+    @staticmethod
+    def create_directory(directory):
+        if len(directory) > 0:
+            if not os.path.exists(directory):
+                if directory[1] == ":":
+                    check_drive = directory[0] + ":\\"
                     if os.path.exists(check_drive):   # TODO os.access
-                        os.makedirs(dir)
+                        os.makedirs(directory)
                     else:
-                        print "ERR drive: ", dir[0], " NOT EXIST !!!"
+                        print "ERR drive: ", directory[0], " NOT EXIST !!!"
             else:
-                print " [INF] directory EXIST, not created:  ", dir, "\n"
+                print " [INF] directory EXIST, not created:  ", directory, "\n"
         else:
-            print " [WRN] directory null name    ", dir, "\n"
+            print " [WRN] directory null name    ", directory, "\n"
 
-    def create_project_working_directory(self, dir):
-        self.create_directory(dir)
+    def create_project_working_directory(self, directory):
+        self.create_directory(directory)
 
-    def create_schema_directory(self, dir):
-        self.create_directory(dir)
-        self.create_directory(dir+"base_setup\\")
-        self.create_directory(dir+"computed_setups\\")
-        self.create_directory(dir+"prevs\\")
-        self.create_directory(dir+"cache\\")
+    def create_schema_directory(self, directory):
+        self.create_directory(directory)
+        self.create_directory(directory + "base_setup\\")
+        self.create_directory(directory + "computed_setups\\")
+        self.create_directory(directory + "prevs\\")
+        self.create_directory(directory + "cache\\")
 
-    def get_setup_ext(self, soft_id):
+    @staticmethod
+    def get_setup_ext(soft_id):
         file_ext = ".null"
         if soft_id == 1:
             #  file_ext = ".hip"   # TODO ext list
@@ -62,7 +67,8 @@ class InOutStorage:
             file_ext = ".max"
         return file_ext
 
-    def get_prev_ext(self, soft_id):
+    @staticmethod
+    def get_prev_ext(soft_id):
         file_ext = ".png"
         if soft_id == 1:
             file_ext = ".png"
@@ -72,38 +78,35 @@ class InOutStorage:
             file_ext = ".png"
         return file_ext
 
-    def getFilesFromDir(self, dir, types="", hack=""):
+    def get_files_from_dir(self, directory, types=""):
         files = []
-        dir_path = self.get_path_from_full(dir)
+        dir_path = self.get_path_from_full(directory)
         if os.path.isdir(dir_path):
-            for file in os.listdir(dir_path):
+            for fi in os.listdir(dir_path):
                 if len(types) > 0:
-                    if file.endswith(types):
-                        files.append(file)
+                    if fi.endswith(types):
+                        files.append(fi)
                 else:
-                    files.append(file)
-
+                    files.append(fi)
         return files
 
-
-
-    def get_files_from_dir_by_object_names(self, dir, obj_list, file_type="", crowd_mode=False,
-                                     crowd_mode_data=["pre", "post", 2, 10]):   # TODO  crowd_mode_data  as class
+    def get_files_from_dir_by_object_names(self, directory, obj_list, file_type="", crowd_mode=False,
+                                           crowd_mode_data=["pre", "post", 2, 10]):   # TODO  crowd_mode_data  as class
         files = []
         crowd = []
         zeros = []
-        dir_path = dir   # TODO  check dir_path = self.get_path_from_full(dir)
+        dir_path = directory   # TODO  check dir_path = self.get_path_from_full(dir)
         if os.path.isdir(dir_path):
-            for file in os.listdir(dir_path):
+            for fi in os.listdir(dir_path):
                 if len(file_type) == 0:
-                    file_name_to_check = file.split(".")[0]
+                    file_name_to_check = fi.split(".")[0]
                 else:
-                    file_name_to_check = file
+                    file_name_to_check = fi
                 for o in obj_list:
                     if len(file_type) > 0:
                         o = o + "." + file_type
                     if o == file_name_to_check:
-                        files.append(file)
+                        files.append(fi)
                         if crowd_mode:
                             crowd.append(o)
                             zeros.append("")
@@ -112,23 +115,22 @@ class InOutStorage:
                         o_name_post = crowd_mode_data[1]
                         nr_zeros = crowd_mode_data[2]
                         for i in range(0, crowd_mode_data[3]):
-                            nrZ = self.comfun.str_with_zeros(i, nr_zeros)
-                            co = o_name_pre + nrZ + o_name_post
+                            nr_z = self.comfun.str_with_zeros(i, nr_zeros)
+                            co = o_name_pre + nr_z + o_name_post
                             if len(file_type) > 0:
                                 co = co + "." + file_type
                             if co == file_name_to_check:
-                                files.append(file)
+                                files.append(fi)
                                 crowd.append(co)
-                                zeros.append(nrZ)
+                                zeros.append(nr_z)
         return [files, crowd, zeros]
 
-
-    def get_frame_range_from_dir(self, dir):    # TODO improve   for fi in os.listdir(dir):
+    def get_frame_range_from_dir(self, directory):    # TODO improve   for fi in os.listdir(dir):
         start = 0
         end = 0
-        if os.path.isdir(dir):
-            for fi in os.listdir(dir):
-                if os.path.isfile(dir+"\\"+fi):
+        if os.path.isdir(directory):
+            for fi in os.listdir(directory):
+                if os.path.isfile(directory + "\\" + fi):
                     fi_no_ext = fi[:-4]
                     fi_arr = fi_no_ext.split("__")
                     if self.comfun.is_float(fi_arr[1]):
@@ -146,10 +148,10 @@ class InOutStorage:
         else:
             proj_working_dir = self.p.current_project.working_directory_absolute
             schema_flat_name = self.get_flat_name(schema_name)
-            dir = proj_working_dir + schema_flat_name + "\\base_setup\\"
-            file = schema_flat_name + "_v" + self.comfun.str_with_zeros(ver, self.p.current_project.zeros_in_version)
+            directory = proj_working_dir + schema_flat_name + "\\base_setup\\"
+            file_version = "_v" + self.comfun.str_with_zeros(ver, self.p.current_project.zeros_in_version)
             file_ext = self.get_setup_ext(self.s.current_soft)
-            return [1, dir + file + file_ext]
+            return [1, directory + schema_flat_name + file_version + file_ext]
 
 
 

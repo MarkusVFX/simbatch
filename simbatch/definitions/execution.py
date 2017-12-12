fb38a5dcf1e4305e15bbd346d4a3ea85eb487c32
MarkusVFX <vfx@mqs.pl>
2017-12-12 15:52:44 +0000

    execution vs executor  .py

---
+++ simbatch/definitions/execution.py
@@ -0,0 +1,94 @@
+
+class Execution:
+    def __init__(self):
+        pass
+        
+    # houdini executions     
+    def houdini_open_scene(self, file):
+        pass
+        
+    def houdini_import_ani(self, objects, dir):
+        pass
+        
+    def houdini_import_cam(self, objects, file_or_dir):
+        pass
+        
+    def houdini_import_obj(self, objects, file_or_dir):
+        pass
+    
+    def houdini_simulate(self, ts, te, objects_names, cache_dir):
+        pass   
+        
+    def houdini_render(self, ts, te, out_file=""):
+        pass 
+        
+    def houdini_save_scene(self, file):
+        pass
+    
+    # 3dsmax executions 
+    def max_open_scene(self, file):
+        pass
+        
+    def max_import_ani(self, objects, dir):
+        pass
+        
+    def max_import_cam(self, objects, file_or_dir):
+        pass
+        
+    def max_import_obj(self, objects, file_or_dir):
+        pass
+    
+    def max_simulate(self, ts, te, objects_names, cache_dir):
+        pass   
+        
+    def max_render(self, ts, te, out_file=""):
+        pass 
+        
+    def max_save_scene(self, file):
+        pass
+    
+    # maya executions
+    def maya_open_scene(self, file):
+        pass
+        
+    def maya_import_ani(self, objects, dir):
+        pass
+        
+    def maya_import_cam(self, objects, file_or_dir):
+        pass
+        
+    def maya_import_obj(self, objects, file_or_dir):
+        pass
+        
+    def maya_set_param(self, object, property, value):
+        pass
+        
+    def maya_simulate_ncloth(self, ts, te, objects_names, cache_dir):
+        pass
+        
+    def maya_simulate_nhair(self, ts, te, objects_names, cache_dir):
+        pass
+        
+    def maya_simulate_nparticle(self, ts, te, objects_names, cache_dir):
+        pass
+        
+    def maya_simulate_fume(self, ts, te, fume_container, cache_dir):
+        pass
+        
+    def maya_simulate_fumewt(self, ts, te, fume_container, cache_dir):
+        pass
+        
+    def maya_render_blast(self, ts, te, out_file=""):
+        pass
+        
+    def maya_render_software(self, ts, te, out_file=""):
+        pass
+        
+    def maya_save_scene(self, file):
+        pass
+        
+    def maya_script_py(self, file):
+        pass
+        
+    def maya_script_mel(self, file):
+        pass 

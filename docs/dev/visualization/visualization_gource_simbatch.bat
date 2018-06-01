bf03bc9e3c25a70cae4a94626478eb0b473f31df
MarkusVFX <vfx@mqs.pl>
2018-06-01 22:46:11 +0000

    visualization + logo

--- docs/dev/visualization/visualization_gource_simbatch.bat
+++ docs/dev/visualization/visualization_gource_simbatch.bat
@@ -2,7 +2,7 @@ gource --hash-seed 5  --date-format "refactoring in progress     %%Y-%%m-%%d %%H
 --bloom-multiplier 1.5 --bloom-intensity 0.6 --caption-offset 40 --multi-sampling -e 0.4 ^
 --font-size 22 --caption-duration 0.1 --caption-colour 7788FF --background 222531 --font-colour 88DDFF --filename-colour FFFFDD --dir-colour FFFFFF  ^
 --frameless  --padding 1.3  --highlight-all-users --stop-at-end --logo-offset 0x0 --logo logo.png --filename-time 2.2  ^
---hide mouse -1280x720 --auto-skip-seconds 0.75 --seconds-per-day 0.55
+--hide mouse -1280x720 --auto-skip-seconds 0.75 --seconds-per-day 0.13
 
 
 REM -f   --title "www.SimBatch.com"    --title "refactoring in progress"

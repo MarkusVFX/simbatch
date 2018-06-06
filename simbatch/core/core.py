683cd69dfc90bd569d6bad5dd695a1cabfc59f0e
Markus Wiertarkus <markus@mqs.pl>
2018-06-06 17:46:00 +0100

    simnodes load on startup

--- simbatch/core/core.py
+++ simbatch/core/core.py
@@ -136,17 +136,19 @@ class SimBatch:
                 if ret_sch is not False:
                     ret_tsk = self.tsk.load_tasks()
                     if ret_tsk is not False:
-                        # count number errors while of loading external data
-                        loading_err_count = 0
-                        loading_err_count = self.check_is_number_of_errors(ret_def, loading_err_count, "definitions")
-                        loading_err_count = self.check_is_number_of_errors(ret_prj, loading_err_count, "project")
-                        loading_err_count = self.check_is_number_of_errors(ret_sch, loading_err_count, "schemas")
-                        loading_err_count = self.check_is_number_of_errors(ret_tsk, loading_err_count, "tsks")
-
-                        if loading_err_count == 0:
-                            return True
+                        ret_nod = self.nod.load_nodes()
+                        if ret_tsk is not False:
+                            loading_err_count = 0     # count number errors while of loading external data
+                            loading_err_count = self.check_is_number_of_errors(ret_def, loading_err_count, "definitions")
+                            loading_err_count = self.check_is_number_of_errors(ret_prj, loading_err_count, "project")
+                            loading_err_count = self.check_is_number_of_errors(ret_sch, loading_err_count, "schemas")
+                            loading_err_count = self.check_is_number_of_errors(ret_tsk, loading_err_count, "tsks")
+                            if loading_err_count == 0:
+                                return True
+                            else:
+                                return loading_err_count
                         else:
-                            return loading_err_count
+                            return -4
                     else:
                         return -1
                 else:

20deeb981b6696ee1fb6231e5e749af4900bfe24
MarkusVFX <vfx@mqs.pl>
2018-06-15 00:44:38 +0000

    gui installer

--- installer/gui_installer.py
+++ installer/gui_installer.py
@@ -1,3 +1,11 @@
+if __name__ == '__main__' and __package__ is None:
+    from os import sys, path
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+from simbatch.core.lib.logger import Logger
+from simbatch.core.lib.common import CommonFunctions
+
+
 try:
     from PySide.QtCore import *
     from PySide.QtGui import *
@@ -55,21 +63,76 @@ class InstallerWindow(QMainWindow):
     window_width = 400
 
     animated_bar = None
+    comfun = None
 
     def __init__(self):
         super(InstallerWindow, self).__init__()
         self.init_ui()
 
     def init_ui(self):
+        self.logger = Logger(log_level=0, console_level=3)
+        self.comfun = CommonFunctions(self.logger)
         self.setMinimumHeight(self.window_height)
         self.setMinimumWidth(self.window_width)
 
         self.animated_bar = AnimatedBar()
 
-        # self.addLayout(self.animated_bar.qt_lay_animated_bar)
         qt_central_widget = QWidget(self)
-        qt_central_widget.setLayout(self.animated_bar.qt_lay_animated_bar)
+        qt_lay_central = QVBoxLayout()
+        qt_lay_central.setContentsMargins(0, 0, 0, 0)
+        qt_central_widget.setLayout(qt_lay_central)
+
+        qt_lay_paths = QVBoxLayout()
+        qt_gbox_paths = QGroupBox()
+        qt_gbox_paths.setLayout(qt_lay_paths)
+        """ SOURCE """
+        qt_lay_path_source = QHBoxLayout()
+        qt_path_source_label = QLabel("Source directory : ")
+        qt_path_source_edit = QLineEdit("")
+        qt_path_source_button = QPushButton("Get")
+        qt_path_source_button.setToolTip('Select directory')
+        qt_path_source_button.clicked.connect(self.on_click_get_source_dir)
+        qt_lay_path_source.addWidget(qt_path_source_label)
+        qt_lay_path_source.addWidget(qt_path_source_edit)
+        qt_lay_path_source.addWidget(qt_path_source_button)
+
+        """ DEST """
+        qt_lay_path_destination = QHBoxLayout()
+        qt_path_destination_label = QLabel("Destination directory : ")
+        qt_path_destination_edit = QLineEdit("")
+        qt_path_destination_button = QPushButton("Get")
+        qt_path_destination_button.setToolTip('Select directory')
+        qt_path_destination_button.clicked.connect(self.on_click_get_source_dir)
+        qt_lay_path_destination.addWidget(qt_path_destination_label)
+        qt_lay_path_destination.addWidget(qt_path_destination_edit)
+        qt_lay_path_destination.addWidget(qt_path_destination_button)
+
+        """  ADD PATHS  (SRC DEST)  """
+        qt_lay_paths.addLayout(qt_lay_path_source)
+        # qt_lay_paths.addSpacerItem(QSpacerItem(20,20))
+        qt_lay_paths.addLayout(qt_lay_path_destination)
+
+
+        qt_lay_buttons = QHBoxLayout()
+        qt_info_button = QPushButton("Info")
+        qt_check_button = QPushButton("Check")
+        qt_install_button = QPushButton("Install")
+
+        qt_lay_buttons.addWidget(qt_info_button)
+        qt_lay_buttons.addWidget(qt_check_button)
+        qt_lay_buttons.addWidget(qt_install_button)
+
+        """  ADD TO CENTRAL  """
+        qt_lay_central.addLayout(self.animated_bar.qt_lay_animated_bar)
+        # qt_lay_central.addLayout(qt_lay_paths)
+        qt_lay_central.addWidget(qt_gbox_paths)
+        qt_lay_central.addLayout(qt_lay_buttons)
         self.setCentralWidget(qt_central_widget)
+
+
+    def on_click_get_source_dir(self):
+        self.comfun.get_dialog_directory(self.qt_settings_data_directory_edit, QFileDialog,
+                                         dir_separator=self.settings.dir_separator)
 
 
 

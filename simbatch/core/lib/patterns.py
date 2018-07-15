155b55a0238289cf601a3aaecd9ea048e7efd62f
MarkusVFX <vfx@mqs.pl>
2018-07-15 23:55:15 +0200

    add def get_evolutions_from_string

--- simbatch/core/lib/patterns.py
+++ simbatch/core/lib/patterns.py
@@ -1,6 +1,7 @@
-import FileSystemOperations
-import CommonFunctions
-import Logger
+from filesystem import FileSystemOperations
+from common import CommonFunctions
+from logger import Logger
+
 
 class Patterns:    # TODO  TESTS !!!!
     patterns = None
@@ -11,6 +12,46 @@ class Patterns:    # TODO  TESTS !!!!
         self.fso = FileSystemOperations()
         self.logger = Logger(log_level=0, console_level=3)
         self.comfun = CommonFunctions(self.logger)
+
+    def get_evolutions_from_string(self, evo_str):
+        total_evos_count = 0
+        out_evos_array = []
+
+        evos_arr = evo_str.split(";")
+
+        evo_count_all = 1
+        for e in evos_arr:  # TODO   optimize !!!
+            e = e.replace("_", "")
+            e_arr = e.split()
+            counter = 0
+            clean_arrr = []
+            evo_count_param_vals = 0
+            passed_abbreviation = False
+            for eee in e_arr:
+                counter += 1
+                if counter > 1:
+                    if passed_abbreviation:
+                        if self.comfun.can_get_float(eee):
+                            exist = 0
+                            for c in clean_arrr:
+                                if c == str(float(eee)):
+                                    exist = 1
+                            if exist == 0:
+                                clean_arrr.append(str(float(eee)))
+                                evo_count_param_vals += 1
+                else:
+                    if len(eee) == 3:
+                        eee = eee.upper()
+                        clean_arrr.append(eee)
+                        passed_abbreviation = True
+
+            if evo_count_param_vals > 0:
+                evo_count_all *= evo_count_param_vals
+
+            if passed_abbreviation:
+                out_evos_array.append(clean_arrr)
+
+        return evo_count_all, out_evos_array
 
     def init_dir_patterns(self):
         patterns = []
@@ -69,8 +110,8 @@ class Patterns:    # TODO  TESTS !!!!
                 pattern_s_n += current_s_n
         return [pattern_s_n, n_count]
 
-    def get_pattern_from_string(self, stri, is_final=False, number_types=["seq", "sh", "take"]):
-        print  "  [db] get_pattern_from_string  ", stri, number_types   # TODO logger
+    def get_pattern_from_string(self, stri, is_final=False, number_types=("seq", "sh", "take")):
+        print "  [db] get_pattern_from_string  ", stri, number_types   # TODO logger
         last_s_n = "."
         pattern_s_n = ""        # pattern type:  snsn
         pattern_final = ""      # seq<nr##>_sh<nr####>    <seq##>_<sh####>
@@ -162,31 +203,31 @@ class Patterns:    # TODO  TESTS !!!!
         most_occurred_pattern = self.check_dir_pattern(level2arr)
         if len(most_occurred_pattern) > 0:
             patt2 = self.generate_final_pattern(level2arr, most_occurred_pattern, 2)
-            full_dir_pattern += "\\" + patt2
+            full_dir_pattern += "\\" + patt2  # TODO os.separator
         patt3 = ""
         most_occurred_pattern = self.check_dir_pattern(level3arr)
         if len(most_occurred_pattern) > 0:
             patt3 = self.generate_final_pattern(level3arr, most_occurred_pattern, 3)
-            full_dir_pattern += "\\" + patt3
+            full_dir_pattern += "\\" + patt3  # TODO os.separator
         print "\n [db]  ret full_dir_pattern     ", full_dir_pattern     # TODO  logger
         return full_dir_pattern
 
     def get_seq_pattern(self, full_dir_pattern):
-        p_arr = full_dir_pattern.split("\\")
+        p_arr = full_dir_pattern.split("\\")  # TODO os.separator
         if len(p_arr) > 0:
             return p_arr[0]
         else:
             return ""
 
     def get_sh_pattern(self, full_dir_pattern):
-        p_arr = full_dir_pattern.split("\\")
+        p_arr = full_dir_pattern.split("\\")  # TODO os.separator
         if len(p_arr) > 1:
             return p_arr[1]
         else:
             return ""
 
     def get_take_pattern(self, full_dir_pattern):
-        p_arr = full_dir_pattern.split("\\")
+        p_arr = full_dir_pattern.split("\\")  # TODO os.separator
         if len(p_arr) > 2:
             return p_arr[2]
         else:

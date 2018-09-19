a19fb665c500c66d00d6fb64fcd527348d458f7b
MarkusVFX <vfx@mqs.pl>
2018-09-19 23:51:35 +0100

    cleanup

--- simbatch/SimBatch_server.py
+++ simbatch/SimBatch_server.py
@@ -5,5 +5,5 @@ simbatch_config_ini = "S:/simbatch/config.ini"
 
 no_gui_batch = simbatch_core.SimBatch("Server", ini_file=simbatch_config_ini)
 
-sim_batch_server = simbatch_server.SimBatchServer(no_gui_batch)
-sim_batch_server.run()
+server = simbatch_server.SimBatchServer(no_gui_batch)
+server.run()

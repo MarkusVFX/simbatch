ed7908651f3f06f7d33c1267c7b0cb53ce959417
MarkusVFX <vfx@mqs.pl>
2018-09-22 22:01:47 +0100

    serewr param ONE or 1

--- simbatch/SimBatch_server.py
+++ simbatch/SimBatch_server.py
@@ -1,7 +1,19 @@
+import sys
 import core.core as simbatch_core
 import server.server as simbatch_server
+
 
 no_gui_batch = simbatch_core.SimBatch("Server", ini_file="config.ini")
 
 server = simbatch_server.SimBatchServer(no_gui_batch)
-server.run()
+if len(sys.argv) > 1:
+    arg = sys.argv[1]
+    if arg == "1" or arg == "one":
+        print "  [INF] run single job : ", len(sys.argv), sys.argv[1]
+        server.run(mode="single")
+    else:
+        print "  [WRN] unknown arg  : ", len(sys.argv), sys.argv[1]
+else:
+    server.run()
+
+

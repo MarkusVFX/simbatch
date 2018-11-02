3920bbd534f81e37691482d5e4a1cbd8ce17d081
MarkusVFX <vfx@mqs.pl>
2018-11-02 05:58:32 +0000

    improve test_add_to_queue.py

--- tests/tests_queue/test_add_to_queue.py
+++ tests/tests_queue/test_add_to_queue.py
@@ -16,78 +16,111 @@ def test_init_simbatch(sib):
     sib.sts.debug_level = 0
     sib.logger.console_level = 0
     #
-
     assert sib.sts.runtime_env == "Stand-alone"
     lo = sib.load_data()
     assert lo[0] is True
 
-    assert sib.prj.total_projects > 0
-    assert sib.sch.total_schemas > 0
-    assert sib.tsk.total_tasks > 0
-    assert sib.que.total_queue_items > 0
-
-
-
 
 def test_set_prj_sch_tsk(sib):
-    assert sib.prj.max_id > 0
-    sib.prj.update_current_from_id(sib.prj.max_id)
-    assert sib.prj.current_project.id == sib.prj.max_id
+    sib.sio.create_unit_tests_example_data(do_save=True)
+    utest_proj_id = sib.prj.get_id_from_name("pytest proj")
+    assert utest_proj_id is not None
+    assert sib.prj.update_current_from_id(sib.prj.max_id) is True
+    assert sib.prj.current_project.project_name == "pytest proj"
 
     sib.dfn.update_current_definition_by_name("Maya")
     sib.sch.update_current_from_id(sib.tsk.tasks_data[-1].schema_id)
     sib.tsk.update_current_from_id(sib.tsk.max_id)
 
-    assert sib.sch.current_schema_id > 0
+    # assert sib.sch.current_schema_id > 0
     assert sib.tsk.current_task_id > 0
 
     sib.que.remove_queue_items(only_done=True)
-
-    # sib.que.print_all()
-    assert sib.que.total_queue_items > 0
-    # sib.que.print_queue_item(sib.que.queue_data[sib.que.total_queue_items-1])
-
-
-def test_get_evolutions_from_string(sib):
-    evos = sib.pat.get_evolutions_from_string("BND 1 2 3")
-    assert evos[0] == 3
-    assert evos[1] == [['BND', '1.0', '2.0', '3.0']]
-
-    evos = sib.pat.get_evolutions_from_string("BND 1 2 3; STR 4 5")
-    assert evos[0] == 6
-    assert evos[1][1] == ['STR', '4.0', '5.0']
 
 
 def test_generate_template_queue_item(sib):
     print "\n\n___ template_queue_item ___"
     template_queue_item = sib.que.generate_template_queue_item(sib.tsk.current_task, sib.sch.current_schema)
+    assert template_queue_item.shot == "ut01"
+    # print " template_queue_item: ", template_queue_item
+    # print " script: ", template_queue_item.evolution_script
+    template_queue_item.print_this()
 
-    # script = sib.que.generate_script_from_Xactions(sib, sib.sch.current_schema)
-    print " template_queue_item: ", template_queue_item
-    # print " script: ", script
-    # template_queue_item.evolution_script = script
-    #template_queue_item.print_this()
+
+def test_generate_template_evo_script(sib):
+    print "\n\n___ template script ___"
+    for i, act in enumerate(sib.sch.current_schema.actions_array):
+        act.actual_value = "test_user_input_" + str(i+1)
+    template_script = sib.que.generate_template_evo_script(sib.sch.current_schema)
+    print template_script
+
+
+def test_get_params_val_arr_from_string(sib):
+    evos_var = sib.pat.get_params_val_arr_from_string("BND 1 2 3")
+    assert evos_var[0] == 3
+    assert evos_var[1] == [['BND', '1.0', '2.0', '3.0']]
+
+    evos_var = sib.pat.get_params_val_arr_from_string("bNd 7 14.5; stR 4.00 5 6")
+    assert evos_var[0] == 6
+    assert evos_var[1][0] == ['BND', '7.0', '14.5']
+    assert evos_var[1][1] == ['STR', '4.0', '5.0', '6.0']
+
+    evos_var = sib.pat.get_params_val_arr_from_string("bNd  7 7 14.5 7 14.5; stR 4.00 5 6  ; MASS  123 4 5; MAS 1 2 3")
+    assert evos_var[0] == 18
+    assert evos_var[1][0] == ['BND', '7.0', '14.5']
+    assert evos_var[1][1] == ['STR', '4.0', '5.0', '6.0']
+    assert evos_var[1][2] == ['MAS', '1.0', '2.0', '3.0']
+
+    # TODO
+    # evos_var = sib.pat.get_params_val_arr_from_string("stR 4.00 5 6  ; ZZZ  123 4 5; MAS 1 2 3")
+    # evos_var = sib.pat.get_params_val_arr_from_string("stR 4.00 5 6  ; ZZZZZ  123 4 5  MAS 1 2 3")
+
+
+def test_get_array_of_scripts_params_val_from_schema_actions(sib):
+    print "\n\n___ scripts_params ___"
+    arr_scripts_params = sib.que.get_array_of_scripts_params_val_from_schema_actions(sib.sch.current_schema)
+    print arr_scripts_params
+
+    evo_action_index = sib.sch.current_schema.get_first_evos_possible()
+    if evo_action_index is not None:
+        sib.sch.current_schema.actions_array[evo_action_index].actual_value = "Bnd 5 55"
+    arr_scripts_params = sib.que.get_array_of_scripts_params_val_from_schema_actions(sib.sch.current_schema)
+    print arr_scripts_params
+
+    evo_action_index = sib.sch.current_schema.get_first_evos_possible()
+    if evo_action_index is not None:
+        sib.sch.current_schema.actions_array[evo_action_index].actual_value = "bND 5 55; MAS 1 2 3"
+    arr_scripts_params = sib.que.get_array_of_scripts_params_val_from_schema_actions(sib.sch.current_schema)
+    print arr_scripts_params
+
+
+def test_do_params_combinations(sib):
+    print "\n\n___ params_combinations ___"
+    arr_scripts_params = sib.que.get_array_of_scripts_params_val_from_schema_actions(sib.sch.current_schema)
+    # print arr_scripts_params
+    all_evo_combinations_array = sib.que.do_params_combinations(arr_scripts_params)
+    print "\n", all_evo_combinations_array
 
 
 def test_generate_queue_items(sib):
     print "\n\n___ queue_items ___"
     qi1 = sib.que.generate_queue_items(sib.tsk.current_task_id)
-    for qi in qi1:
-        print "___"
-        qi.print_this()
+    # for qi in qi1:
+    #     print "___"
+    #     qi.print_this()
 
 
-def test_generate_queue_items(sib):
-    print "\n\n___ queue_items ___"
+def test_generate_queue_items_evo(sib):
+    print "\n\n___ queue_items with evo ___"
     sib.sch.update_current_from_id(sib.tsk.tasks_data[-1].schema_id)
     sib.tsk.update_current_from_id(sib.tsk.max_id)
-    sib.print_important_values()
+    # sib.print_important_values()
 
     custom_action_inputs = [["ooppoo"], [["sim_clt"], "DMP 1 2 3"], ["simed_file"]]
     # TO DO !!! , action_inputs = custom_action_inputs
     qi1 = sib.que.generate_queue_items(sib.tsk.current_task_id)
-    for qi in qi1:
-        print "___"
-        qi.print_this()
+    # for qi in qi1:
+    #     print "___"
+    #     qi.print_this()
 
 

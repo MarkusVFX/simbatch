4d06c275c7a97e6f8c09f85e83600b7d72a536f0
MarkusVFX <vfx@mqs.pl>
2018-11-06 21:36:47 +0000

    improved stand-alone  definitoion

--- simbatch/definitions/interactions_standalone.py
+++ simbatch/definitions/interactions_standalone.py
@@ -7,8 +7,32 @@ class Interactions:
         self.logger = logger
 
     def print_info(self):
-        self.logger.raw("This is interaction with Maya")
+        self.logger.raw("This is stand-alone's interactions")
 
     # framework interactions
     def schema_item_double_click(self, param):
-        self.logger.raw(("Double click on schema item", param))
+        self.logger.raw("[INF interactions] Double clicked on stand-alone schema item (defined in interactions file)")
+        
+    def sa_loop_over_files(self, file):
+        return ret2
+        
+        
+    def sa_loop_over_dir(self, file):
+        import maya.cmds as cmd
+        ret1 = cmd.file(rename=target)
+        ret2 = cmd.file(save=True, type="mayaBinary")
+        return ret2
+        
+    def simple_info_print(self):
+        print "\n\n simple info test "
+        
+    def detailed_info_print(self):
+        print "\n\n detailed info test "
+        
+    def reset_filter(self):
+        return "*.*"
+        
+    def reset_details(self):
+        return "*.*|time|date|size"
+       
+    

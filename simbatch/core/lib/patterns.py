b8f0dc377ae90b2a109b777b79dcd429f969c759
MarkusVFX <vfx@mqs.pl>
2018-12-08 22:54:19 +0000

    remove fileststem.py

--- simbatch/core/lib/patterns.py
+++ simbatch/core/lib/patterns.py
@@ -1,6 +1,20 @@
-from filesystem import FileSystemOperations
 from common import CommonFunctions
 from logger import Logger
+
+
+class FileSystemOperations:
+    arr_dirs = []  # TODO optimize   id: 12
+    max_dir_depth = 0
+
+    def get_dir_depth(self, dir, level=1):
+        if os.path.isdir(dir):
+            for d in os.listdir(dir):
+                if level > self.max_dir_depth:
+                    self.max_dir_depth = level
+                dir_name = os.path.join(dir, d)
+                if os.path.isdir(dir_name):
+                    self.arr_dirs.append([level, d, dir_name])
+                    self.get_dir_depth(dir_name, level + 1)
 
 
 class Patterns:    # TODO  TESTS !!!!

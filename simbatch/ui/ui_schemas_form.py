4e74e368a2329ee2500efea3cb94f16f91c2b596
MarkusVFX <vfx@mqs.pl>
2019-02-04 23:41:31 +0000

    if schema name exists change color to RED

--- simbatch/ui/ui_schemas_form.py
+++ simbatch/ui/ui_schemas_form.py
@@ -84,7 +84,8 @@ class SchemaFormCreateOrEdit(QWidget):
 
         # fae   form add/edit
         qt_fae_schema_name = EditLineWithButtons("Name: ", label_minimum_size=60)
-        self.qt_fae_schema_name_edit = qt_fae_schema_name.qt_edit_line
+        self.qt_fae_schema_name_edit = qt_fae_schema_name    # if sch name exist -> color RED
+        self.qt_fae_schema_name_edit.qt_edit_line.textChanged.connect(self.on_change_sch_name)
         qt_fae_schema_description = EditLineWithButtons("Description:  ", label_minimum_size=60)
         self.qt_fae_schema_description_edit = qt_fae_schema_description.qt_edit_line
         qt_fae_schema_version = EditLineWithButtons("Version:  ", label_minimum_size=55)
@@ -325,6 +326,15 @@ class SchemaFormCreateOrEdit(QWidget):
         else:
             self.qt_bg_schema_top.setTitle("Edit Schema : " + txt)
 
+    def on_change_sch_name(self, name):
+        for s in self.batch.sch.schemas_data:
+            if name == s.schema_name:
+                self.qt_fae_schema_name_edit.label.setText("Name !")
+                self.qt_fae_schema_name_edit.label.setStyleSheet("""color:#FF0000;font-weight: bold;""")
+                return True
+        self.qt_fae_schema_name_edit.label.setText("Name")
+        self.qt_fae_schema_name_edit.label.setStyleSheet("""color:#000000""")
+
     def on_edit_schema_description(self, txt):
         self.local_schema_item.description = txt
 
@@ -347,7 +357,7 @@ class SchemaFormCreateOrEdit(QWidget):
 
     """  on show form create / edit / change item """
     def update_local_schema_item(self, schema_item):
-        self.qt_fae_schema_name_edit.setText(schema_item.schema_name)
+        self.qt_fae_schema_name_edit.qt_edit_line.setText(schema_item.schema_name)
         self.qt_fae_schema_version_edit.setText(str(schema_item.schema_version))
         self.qt_fae_schema_description_edit.setText(schema_item.description)
         self.local_schema_item = copy.deepcopy(schema_item)

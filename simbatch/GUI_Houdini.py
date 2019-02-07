16962a287608bcb1a677f67641ff27dc908363ae
MarkusVFX <vfx@mqs.pl>
2019-02-07 21:43:08 +0000

    parent window to Houdini

--- simbatch/GUI_Houdini.py
+++ simbatch/GUI_Houdini.py
@@ -10,20 +10,23 @@ simbatch_config_ini = "S:/simbatch/config.ini"
 ##
 #
 
+import hou
 import sys
+
 sys.path.append(simbatch_instalation_dir)
 
 import core.core as simbatch_core
 import simbatch.server.server as simbatch_server
 import ui.mainw as simbatch_ui
 
-    
+from PySide2 import QtCore, QtUiTools, QtWidgets
+
 simbatch = simbatch_core.SimBatch("Houdini", ini_file=simbatch_config_ini)
 loading_data_state = simbatch.load_data()
 simbatch_server = simbatch_server.SimBatchServer(simbatch, framework_mode=True)
 
-
 if simbatch.sts.with_gui == 1:
     main_window = simbatch_ui.MainWindow(simbatch_server)
+    main_window.setParent(hou.ui.mainQtWindow(), QtCore.Qt.Window)
     main_window.show()
     main_window.post_run(loading_data_state)

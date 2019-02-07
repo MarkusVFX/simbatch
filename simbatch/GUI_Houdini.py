6fc373a3325bf02e517d29bfe96cde4485c06815
MarkusVFX <vfx@mqs.pl>
2019-02-07 21:12:47 +0000

    houdini init window->server->simbatch

--- simbatch/GUI_Houdini.py
+++ simbatch/GUI_Houdini.py
@@ -14,14 +14,16 @@ import sys
 sys.path.append(simbatch_instalation_dir)
 
 import core.core as simbatch_core
+import simbatch.server.server as simbatch_server
 import ui.mainw as simbatch_ui
 
     
-sim_batch = simbatch_core.SimBatch("Houdini", ini_file=simbatch_config_ini)
-loading_data_state = sim_batch.load_data()
+simbatch = simbatch_core.SimBatch("Houdini", ini_file=simbatch_config_ini)
+loading_data_state = simbatch.load_data()
+simbatch_server = simbatch_server.SimBatchServer(simbatch, framework_mode=True)
 
 
-if sim_batch.sts.with_gui == 1:
-    main_window = simbatch_ui.MainWindow(sim_batch) 
+if simbatch.sts.with_gui == 1:
+    main_window = simbatch_ui.MainWindow(simbatch_server)
     main_window.show()
     main_window.post_run(loading_data_state)

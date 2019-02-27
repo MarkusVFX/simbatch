8d9c660b664846943f1be4f78f4c0b20885f4382
MarkusVFX <vfx@mqs.pl>
2019-02-27 03:10:29 +0000

    improve set_param

--- simbatch/definitions/interactions_maya.py
+++ simbatch/definitions/interactions_maya.py
@@ -27,6 +27,9 @@ class Interactions:
     def save_setup_as_next_version(self, param):
         self.save_current_scene_as(param)
 
+    def set_param(self, objects, property, value):
+        self.maya_set_param(objects, property, value)
+
     # maya interactions
     def maya_open_scene(self, target):
         self.logger.int(("maya_open_scene", target))
@@ -43,6 +46,7 @@ class Interactions:
 
     def save_current_scene_as(self, target_file):
         target_dir = self.comfun.dirname(target_file)
+        self.logger.db((" target_dir: ", str(target_dir)))
         self.comfun.create_directory_if_not_exists(target_dir)
         if self.comfun.path_exists(target_dir):
             self.comfun.create_directory(target_dir)
@@ -87,8 +91,12 @@ class Interactions:
     def maya_import_obj(self, objects, file_or_dir):
         self.logger.int(("maya_import_obj", objects, file_or_dir))
         
-    def maya_set_param(self, object, property, value):
-        self.logger.int(("maya_set_param", object, property, value))
+    def maya_set_param(self, objects, abbrev_param, value):
+        import maya.cmds as cmd
+        for obj in objects.split(","):
+            param_full_name = abbrev_param
+            cmd.setAttr(obj+"."+param_full_name, value)
+        self.logger.db(("maya_set_param", object, property, value), nl=True)
         
     def maya_simulate_ncloth(self, ts, te, objects_names, cache_dir, cache_mode=1, cache_subsamples=1):
         self.logger.int(("maya_simulate_ncloth", ts, te, objects_names, cache_dir))
@@ -109,7 +117,7 @@ class Interactions:
         cmds.playbackOptions(minTime=fr_start)
         cmds.playbackOptions(maxTime=fr_end)
         try:
-            cmd = 'select -r ' + objects_names + ' ; viewFit;'
+            cmd = 'select -r ' + objects_names.replace(",", " ")    #  TODO ' ; viewFit;'
             self.logger.inf(cmd, nl=True, nl_after=True)
             ml.eval(cmd)
 
@@ -161,12 +169,15 @@ class Interactions:
         cmds.playbackOptions(maxTime=fr_end)
 
         if len(out_file) > 0:
-            base_name = self.comfun.basename(out_file)
-            out_dir = self.comfun.dirname(out_file)
-            out_file_header = base_name.split(".")[0]
-            out_file_ext = base_name.split(".")[1]
-            print " [db] SimC Rend !   ",  out_dir,  out_file_header,  out_file_ext
-            #cmds.playblast(f=outFile, st=int(ts), et=int(te), format='qt', compression='H.264', framePadding=4, percent=100, wh=[1920, 1080])
+            # base_name = self.comfun.basename(out_file)
+            # out_dir = self.comfun.dirname(out_file)
+            # out_file_header = base_name.split(".")[0]
+            # out_file_ext = base_name.split(".")[1]
+            # print " [db] SimC Rend !   ",  out_dir,  out_file_header,  out_file_ext
+            out_file = out_file.replace("####", "")
+            out_file = out_file[:-5]
+            self.logger.db((" render out file: ",  out_file))
+            # cmds.playblast(f=outFile, st=int(ts), et=int(te), format='qt', compression='H.264', framePadding=4, percent=100, wh=[1920, 1080])
             cmds.playblast(f=out_file, st=int(ts), et=int(te), format='image', compression='jpg', framePadding=4,
                            percent=100, wh=[1280, 720])
         else:
@@ -192,7 +203,15 @@ class Interactions:
 
     def get_objects_by_type(self, type):
         if type == 'nCloth':
-            return self.get_cloth_objects()
+            objs = self.get_cloth_objects()
+            self.logger.inf("Detected ({}) nCloth objects:".format(len(objs), objs))
+
+        objs_str = ""
+        for obj in objs:
+            objs_str += obj + ","
+        if len(objs_str) > 0:
+            objs_str = objs_str[:-1]
+        return objs_str
 
     def get_cloth_objects(self):
         import maya.cmds as cmd
